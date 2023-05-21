66d9b7acd1dc38f049b042428881f639e93ddf8b
ShivaniG1917 <76456581+ShivaniG1917@users.noreply.github.com>
2023-05-21 06:39:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function add() {
+    var result =
+    Number(document.getElementById("firstno").value)+
+    Number(document.getElementById("secondno").value);    
+    console.log(result);    
+    }
+    function sub() {
+    var result =
+    Number(document.getElementById("firstno").value)-
+    Number(document.getElementById("secondno").value);    
+    console.log(result);    
+    }
+    function mul() {
+    var result =
+    Number(document.getElementById("firstno").value)*
+    Number(document.getElementById("secondno").value);    
+    console.log(result);    
+    }
+    function div() {
+    var result =
+    Number(document.getElementById("firstno").value)/
+    Number(document.getElementById("secondno").value);    
+    console.log(result);    
+    }
